9d5c0e9a756431bc37f53373aca58d49ac3c1be5
conradcorbett <conrad.corbett@hashicorp.com>
2023-07-07 15:47:35 -0700

    testing agents

--- main.tf
+++ main.tf
@@ -24,3 +24,12 @@ resource "docker_container" "nginx" {
     external = 8000
   }
 }
+
+resource "docker_container" "nginx2" {
+  image = docker_image.nginx.name
+  name  = "nginx2"
+  ports {
+    internal = 80
+    external = 8002
+  }
+}
